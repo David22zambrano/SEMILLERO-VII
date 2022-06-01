49490b9f8b8d2e050cb858d16f3af93bf8152c6d
David Sebastian <David Sebastian Pea Zambrano>
2022-06-01 17:03:11 -0500

    upload

--- Proyectos/Portal TI Exercise/src/script.js
+++ Proyectos/Portal TI Exercise/src/script.js
@@ -2,6 +2,9 @@ const expReg = {
   name: /^[a-zA-Z0-9\_\-]{4,16}$/,
   name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
   email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  phoneNumber: /^[2-9]\d{2}[2-9]\d{2}\d{4}$/,
+  nitCompany: /^([0-9]{7,15}-[0-9]{1})?$/,
+  description: /^[\s\S]{10,100}$/,
 };
 const inputs = document.getElementsByTagName("input");
 let arrayInputs = Array.from(inputs);
@@ -13,4 +16,3 @@ function validateForm({ target }) {
     ? target.style.border = "2px solid green"
     : target.style.border = "2px solid red"
 }
-  
