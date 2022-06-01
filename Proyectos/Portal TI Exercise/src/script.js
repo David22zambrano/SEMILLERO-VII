502e6c78b9a86bddd5972f99284d4f2cb38054a9
David Sebastian <David Sebastian Pea Zambrano>
2022-06-01 14:38:12 -0500

    last Upload

---
+++ Proyectos/Portal TI Exercise/src/script.js
@@ -0,0 +1,17 @@
+const expReg = {
+  name: /^[a-zA-Z0-9\_\-]{4,16}$/,
+  name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  password: /^.{4,12}$/,
+};
+const inputs = document.getElementsByTagName("input");
+let arrayInputs = Array.from(inputs);
+arrayInputs.forEach((input) => {
+  input.addEventListener("keyup", validateForm);
+});
+function validateForm({ target }) {
+  expReg[`${target.name}`].test(target.value)
+    ? target.style.border = "2px solid green"
+    : target.style.border = "2px solid red"
+}
+  
