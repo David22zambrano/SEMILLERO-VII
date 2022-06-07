737aa7eea697883fdaa2997b9cb7fc147ef34a81
David Sebastian <David Sebastian Pea Zambrano>
2022-06-07 17:45:43 -0500

    upload 7 june 2022

--- Proyectos/3 Subir extracto bancario/src/script.js
+++ Proyectos/3 Subir extracto bancario/src/script.js
@@ -1,15 +1,31 @@
+const fecha = document.getElementById("fecha");
+const inputs = document.querySelectorAll("input");
+const image = document.getElementById("btn");
+const currencySelect = document.getElementById("currencySelect");
 
-const fecha = document.getElementById('fecha');
-const inputs = document.querySelectorAll('input');
-const image = document.getElementById('btn');
+image.addEventListener("click", () => {
+  image.classList.toggle("btn-act");
+});
 
-image.addEventListener('click',()=>{
-    image.classList.toggle('btn-act');
-})
-
-let arrayInputs = Array.from(inputs);
-arrayInputs.forEach((input) => {
-    input.addEventListener("click", validaInput);
+let today = new Date();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+let date = today.getDate();
+let hours = addZero(today.getHours());
+let minutes = addZero(today.getMinutes());
+let seconds = addZero(today.getSeconds());
+let current_date = `${month}/${date}/${year}:${hours}:${minutes}:${seconds}`;
+fecha.innerText = current_date;
+function addZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
+fetch("./data/playData.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(Array.from(data));
+    Object.keys(data).forEach((currency) => {
+      let option = document.createElement("option");
+      option.innerText = currency;
+      currencySelect.append(option);
+    });
   });
-
-fecha.textContent =  new Date(Date.now()).toISOString();
