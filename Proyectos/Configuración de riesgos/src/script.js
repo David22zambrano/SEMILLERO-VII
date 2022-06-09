cfffbb9ebb34a45be7035b3d252287e27bd99512
David Sebastian <David Sebastian Pea Zambrano>
2022-06-09 16:19:29 -0500

    upload

--- Proyectos/Configuración de riesgos/src/script.js
+++ Proyectos/Configuración de riesgos/src/script.js
@@ -1,1 +1,15 @@
+const expReg = {
+    name: /^[a-zA-Z0-9\_\-]{4,16}$/
+}
+
 const addItems = document.querySelector('#button_add_new');
+const buttons_navegation = document.querySelectorAll('button');
+
+
+
+buttons_navegation.addEventListener("click", () => {
+    buttons_navegation.classList.toggle("btn-act");
+  });
+
+
+
