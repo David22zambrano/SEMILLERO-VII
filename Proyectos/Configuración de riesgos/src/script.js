a88e4141f047820908a8df454cefbb2cd73c3a4a
David Sebastian <David Sebastian Pea Zambrano>
2022-06-14 17:16:21 -0500

    upload exercise 10

--- Proyectos/Configuración de riesgos/src/script.js
+++ Proyectos/Configuración de riesgos/src/script.js
@@ -1,18 +1,38 @@
-
-const poop = document.getElementById("poop");
-const form = document.getElementById('form_Add_New');
+const expReg = {
+  descripcion: /^[\s\S]{5,25}$/
+};
+const pop = document.getElementById("pop");
+const form = document.getElementById("form_Add_New");
 const button_Open_Form = document.getElementById("form_Add_New");
 const button_Close_Form = document.getElementById("close_Window_Add_New");
 const submit = document.getElementById("button_Guardar");
 const tbody = document.getElementById("tbody");
-const items = JSON.parse(localStorage.getItem("items")) || [];
+let items = JSON.parse(localStorage.getItem("items")) || [];
+const table_Aside_Right = document.querySelector('#right');
+const tableView = document.querySelector("table");
+const button_add_new = document.getElementById("button_add_new");
+const header_table = document.getElementById("impactos");
+const buttons_Navegation = [...document.querySelectorAll('.buttons_nav')];
 
-open_Form = () => (poop.style.display = "flex");
-close_Form = () => (poop.style.display = "none");
+open_Form = () => (pop.style.display = "flex");
+close_Form = () => (pop.style.display = "none");
+buttons_Navegation.forEach(btn => btn.addEventListener('click',(e)=>{
+  toggleHeader(e)
+}))
 
-mapElementList(items, tbody);
+toggleHeader(items, tbody);
 
-function addItem (e)  {
+const inputsForm = document.getElementsByTagName("label");
+let arrayInputs = Array.from(inputsForm);
+arrayInputs.forEach((label) => {
+  label.addEventListener("keyup", validateForm);
+});
+function validateForm({target}) {
+  expReg[`${target.name}`].test(target.value)
+    ? (target.style.border = "2px solid green")
+    : (target.style.border = "2px solid red");
+}
+function addItem(e) {
   e.preventDefault();
   const description = this.querySelector("[name=descripcion]").value;
   const valMin = this.querySelector("[name=valorMinimo]").value;
@@ -20,32 +40,42 @@ function addItem (e)  {
   const item = {
     description,
     valMin,
-    valMax
+    valMax,
   };
   items.push(item);
-  localStorage.setItem("items", JSON.stringify(items));
+  localStorage.setItem(document.querySelector('h2').innerText,JSON.stringify(items));
   location.reload();
   let resetForm = document.querySelector("form").reset();
+  validateForm();
 }
 
-function mapElementList(elements, tbody)  {
-    console.log(elements);
-  tbody.innerHTML = elements.map((element, i) => {
-      return `
-        <tr>
-            <td>${element.description}</td>
-            <td>${element.valMin}</td>
-            <td>${element.valMax}</td>
+function toggleHeader({target}){
+ document.querySelector('h2').innerText = target.innerText;
+ items = JSON.parse(localStorage.getItem(target.innerText)) || [];
+ console.log(items);
+ tbody.innerHTML = items
+    .map((item, i) => {
+      return ` 
+      <tr>
+            <td>${item.description}</td>
+            <td>${item.valMin}</td>
+            <td>${item.valMax}</td>
             <td>
-                <img src="./img/pencil-edit.svg"">
-                <img src="./img/trash-svgrepo-com (1).svg">
+            <img src="./src/img/editar.svg" alt="Edit">
+            <img src="./src/img/eliminar.svg" alt="Delete">
             </td>
         </tr>
         `;
-    }).join("");
+    })
+    .join("");
 }
-poop.addEventListener('submit', addItem);
 
-toggleViewTable = () =>{
-  
-} 
+
+const createEl = (tagName, clase, texto, parent) => {
+  let element = document.createElement(`${tagName}`);
+  element.classList.add(`${clase}`);
+  element.innerText = `${texto}`;
+  parent.append(element);
+};
+pop.addEventListener("submit", addItem);
+
