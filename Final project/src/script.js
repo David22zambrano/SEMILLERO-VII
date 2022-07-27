fcf6d4694331a954455cd1f548298709ff05e8de
David22zambrano <david.zambrano17@outlook.com>
2022-07-27 18:07:43 -0500

    upload modulo text

--- Final project/src/script.js
+++ Final project/src/script.js
@@ -1,3 +1,5 @@
+import {MDCTextField} from '@material/textfield';
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
 import lineaPago from '../data/lineaPago';
 import lineaDias from '../data/lineaDias';
