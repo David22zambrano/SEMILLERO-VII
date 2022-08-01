c587a4b6221705d79365e8759938e3c191df28a3
David22zambrano <david.zambrano17@outlook.com>
2022-08-01 16:01:11 -0500

    upload 01/07/2022 16:00pm

--- contenido/src/script.js
+++ contenido/src/script.js
@@ -4,3 +4,6 @@ import {MDCTopAppBar} from '@material/top-app-bar';
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
+import {MDCRipple} from '@material/ripple';
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
