86530a99da75a449021e0fec6094cb0651e6e8e6
David22zambrano <david.zambrano17@outlook.com>
2022-08-01 09:03:41 -0500

    upload

--- Final project/src/script.js
+++ Final project/src/script.js
@@ -1,5 +1,9 @@
 import {MDCTextField} from '@material/textfield';
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+import {MDCTopAppBar} from '@material/top-app-bar';
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 import lineaPago from '../data/lineaPago';
 import lineaDias from '../data/lineaDias';
