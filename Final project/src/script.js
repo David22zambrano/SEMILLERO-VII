fc76823767e1d85c908b6a66f4adc51e36219370
David22zambrano <david.zambrano17@outlook.com>
2022-08-02 14:55:47 -0500

    upload 02/08/2022 2:55pm

--- Final project/src/script.js
+++ Final project/src/script.js
@@ -1,17 +1,17 @@
-import {MDCTextField} from '@material/textfield';
-const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
-import {MDCTopAppBar} from '@material/top-app-bar';
+// import {MDCTextField} from '@material/textfield';
+// const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+// import {MDCTopAppBar} from '@material/top-app-bar';
 // Instantiation
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
+// const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+// const topAppBar = new MDCTopAppBar(topAppBarElement);
 
-import lineaPago from '../data/lineaPago';
-
-import lineaDeDias from "./data/lineaDias";
-
-    const createBoxDay =  lineaDeDias.map(function(){
+// import lineaPago from '../data/lineaPago';
+import lineaDeDias from "../data/lineaDias";
+const 
+const createBoxDay =  lineaDeDias.map(()=>{
         lineaDeDias.forEach(day => {
-            document.createElement('div')
+            document.createElement('div').style('width: 20px, heigth: 20px')
         });
+        console.log(lineaDeDias.dias[1])
         
     })
